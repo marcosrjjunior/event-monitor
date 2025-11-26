10132bba19f54a291652539eb8b32d59fcb2f8af
marcosrjjunior <marcosrjjunior@gmail.com>
2025-11-26 19:12:16 +1000

    bump up dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default async function Home({ searchParams }) {
   const { user_id } = await searchParams
 
   return (
-    <div className="bg-base-100 min-h-screen gap-2 p-8 pb-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="bg-base-100 min-h-screen gap-2 p-8 pb-20 font-(family-name:--font-geist-sans)">
       <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
         <div className="flex w-full flex-row flex-wrap gap-5">
           <Suspense>
--- src/app/reports/page.tsx
+++ src/app/reports/page.tsx
@@ -2,7 +2,7 @@ import Charts from '@/components/Charts/Charts'
 
 export default async function ChartsPage() {
   return (
-    <div className="bg-base-100 min-h-screen gap-2 p-8 pb-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="bg-base-100 min-h-screen gap-2 p-8 pb-20 font-(family-name:--font-geist-sans)">
       <main className="">
         <span>
           **This page still need some work. We are still using hardcoded data
